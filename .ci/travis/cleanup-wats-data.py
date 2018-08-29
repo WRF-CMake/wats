56b3f1723d06380ac74d2960010c44571a5123b4
Maik Riechert <maik.riechert@arcor.de>
2018-08-29 22:23:12 +0100

    add script for cleaning up old wats-data branches

---
+++ .ci/travis/cleanup-wats-data.py
@@ -0,0 +1,34 @@
+# Copyright 2018 M. Riechert and D. Meyer. Licensed under the MIT License.
+
+from typing import Iterable
+import os
+from datetime import datetime, timedelta
+
+import github
+
+EXCLUDE = ['master']
+KEEP_NEWEST = 5
+REMOVE_AFTER = timedelta(days=1)
+DRY_RUN = False
+
+def get_date_utc(branch: github.Branch.Branch) -> datetime:
+    return branch.commit.commit.author.date
+
+g = github.Github(os.environ['GITHUB_TOKEN'])
+repo = g.get_repo(os.environ['WATS_DATA_REPO'])
+all_branches = repo.get_branches()
+data_branches = filter(lambda branch: branch.name not in EXCLUDE, all_branches)
+sorted_branches = sorted(data_branches, key=get_date_utc)
+
+for i, branch in enumerate(sorted_branches):
+    if i >= len(sorted_branches) - KEEP_NEWEST:
+        print(f'Keeping {branch.name} (within newest {KEEP_NEWEST} branches)')
+        continue
+    age = datetime.utcnow() - get_date_utc(branch)
+    if age < REMOVE_AFTER:
+        print(f'Keeping {branch.name} (age={age} < {REMOVE_AFTER})')
+        continue
+    print(f'Deleting {branch.name} (age={age} >= {REMOVE_AFTER})')
+    if not DRY_RUN:
+        ref = repo.get_git_ref('heads/' + branch.name)
+        ref.delete()
