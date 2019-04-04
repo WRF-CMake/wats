9bcb33d8527683126274af56cfb4dbcff3abb52d
Maik Riechert <maik.riechert@arcor.de>
2019-04-04 22:12:30 +0100

    detail plots

--- wats/ext_boxplot.py
+++ wats/ext_boxplot.py
@@ -7,20 +7,22 @@ from matplotlib.markers import MarkerStyle
 from matplotlib.patches import Rectangle
 from matplotlib.collections import LineCollection
 
-class ExtendedBoxplotStats(object):
-    def __init__(self, array, label=None, percentiles=[0.1, 1, 5, 25, 75, 95, 99, 99.9]):
-        array = np.asanyarray(array)
-        self.total = array.size
-        self.median = np.median(array)
-        self.mean = np.mean(array)
-        self.percentiles = np.percentile(array, percentiles)
-        self.min = np.min(array)
-        self.max = np.max(array)
-        self.outliers_min = array[array < self.percentiles[0]]
-        self.outliers_max = array[array > self.percentiles[-1]]
-        self.label = label
+def compute_extended_boxplot_stats(array, label=None, percentiles=[0.1, 1, 5, 25, 75, 95, 99, 99.9]) -> dict:
+    array = np.asanyarray(array)
+    p = np.percentile(array, percentiles)
+    return {
+        'total': array.size,
+        'median': np.median(array),
+        'mean': np.mean(array),
+        'percentiles': p,
+        'min': np.min(array),
+        'max': np.max(array),
+        'outliers_min': array[array < p[0]],
+        'outliers_max': array[array > p[-1]],
+        'label': label
+    }
 
-def plot_extended_boxplot(ax: Axes, boxplot_stats: List[ExtendedBoxplotStats],
+def plot_extended_boxplot(ax: Axes, boxplot_stats: List[dict],
                           positions: Optional[List[float]]=None,
                           showmeans: bool=True,
                           manage_xticks: bool=True) -> None:
@@ -46,20 +48,22 @@ def plot_extended_boxplot(ax: Axes, boxplot_stats: List[ExtendedBoxplotStats],
         return boxplot_x0
 
     for stats, position in zip(boxplot_stats, positions):
-        labels.append(stats.label if stats.label is not None else position)
+        labels.append(stats['label'] if stats['label'] is not None else position)
 
-        print('{}: min: {}, max: {}, mean: {}, median: {}, #outliers_min={}, #outliers_max={}, #total={}'.format(
-            labels[-1], stats.min, stats.max, stats.mean, stats.median,
-            len(stats.outliers_min), len(stats.outliers_max), stats.total
-            ))
+        percentiles = stats['percentiles']
+
+        #print('{}: min: {}, max: {}, mean: {}, median: {}, #outliers_min={}, #outliers_max={}, #total={}'.format(
+        #    labels[-1], stats['min'], stats['max'], stats['mean'], stats['median'],
+        #    len(stats['outliers_min']), len(stats['outliers_max']), stats['total']
+        #    ))
 
         # Compute the number of iterations based on the number of percentiles
-        percentiles_num_iter = int(len(stats.percentiles)/2) # List of percentiles are always pairs so always a multiple of 2
+        percentiles_num_iter = int(len(percentiles)/2) # List of percentiles are always pairs so always a multiple of 2
 
         boxplot_width = 0.3
         for index in range(percentiles_num_iter):
-            r = Rectangle((get_boxplot_x0(position, boxplot_width), stats.percentiles[index]), 
-                        boxplot_width, stats.percentiles[-index-1] - stats.percentiles[index], 
+            r = Rectangle((get_boxplot_x0(position, boxplot_width), percentiles[index]), 
+                        boxplot_width, percentiles[-index-1] - percentiles[index], 
                         facecolor=boxplot_facecolors[index], fill=True, edgecolor='k')
             ax.add_patch(r)
             if index + 1 < percentiles_num_iter:
@@ -68,36 +72,36 @@ def plot_extended_boxplot(ax: Axes, boxplot_stats: List[ExtendedBoxplotStats],
         x0 = get_boxplot_x0(position, boxplot_width)
         x1 = x0 + boxplot_width
 
-        ax.hlines(stats.median, x0, x1, colors='r', linestyles='solid')
+        ax.hlines(stats['median'], x0, x1, colors='r', linestyles='solid')
         
         if showmeans:
-            ax.plot(position, stats.mean, marker='o', markersize=3, color="k")
+            ax.plot(position, stats['mean'], marker='o', markersize=3, color="k")
 
         # Anything which is above the min and max range of percentiles,
         # we 'squash' into two lines which are half the percentile-range away
         # from the min and max percentiles.
-        dist_outliers = (stats.percentiles.max() - stats.percentiles.min()) / 2
+        dist_outliers = (percentiles.max() - percentiles.min()) / 2
 
         blue = '#67a9cf'
 
         # For the bottom
-        bottom_y = stats.percentiles[0] - dist_outliers
-        bottom_y_half = stats.percentiles[0] - dist_outliers / 2
+        bottom_y = percentiles[0] - dist_outliers
+        bottom_y_half = percentiles[0] - dist_outliers / 2
         ax.hlines(bottom_y, x0, x1, colors=blue, linestyles='solid')
-        ax.vlines(position, bottom_y, stats.percentiles[0], colors=blue, linestyles='dashed')
-        ax.annotate('{:6.2f}'.format(stats.min), 
+        ax.vlines(position, bottom_y, percentiles[0], colors=blue, linestyles='dashed')
+        ax.annotate('{:6.2f}'.format(stats['min']), 
                     xy=(position, bottom_y - abs(bottom_y)*0.01), ha='center', va='top', color=blue)
-        ax.annotate('N={}'.format(len(stats.outliers_min)),
+        ax.annotate('N={}'.format(len(stats['outliers_min'])),
                     xy=(position + 0.005, bottom_y_half), rotation=90, va='center', color=blue)
 
         # For the top
-        top_y = stats.percentiles[-1] + dist_outliers
-        top_y_half = stats.percentiles[-1] + dist_outliers / 2
+        top_y = percentiles[-1] + dist_outliers
+        top_y_half = percentiles[-1] + dist_outliers / 2
         ax.hlines(top_y, x0, x1, colors=blue, linestyles='solid')
-        ax.vlines(position, top_y, stats.percentiles[-1], colors=blue, linestyles='dashed')
-        ax.annotate('{:6.2f}'.format(stats.max), 
+        ax.vlines(position, top_y, percentiles[-1], colors=blue, linestyles='dashed')
+        ax.annotate('{:6.2f}'.format(stats['max']), 
                     xy=(position, top_y), ha='center', va='bottom', color=blue)
-        ax.annotate('N={}'.format(len(stats.outliers_max)),
+        ax.annotate('N={}'.format(len(stats['outliers_max'])),
                     xy=(position + 0.005, top_y_half), rotation=90, va='center', color=blue)
 
     if manage_xticks:
@@ -127,7 +131,7 @@ if __name__ == '__main__':
 
     #plt.boxplot(dist_norm)
 
-    boxplot_stats = ExtendedBoxplotStats(dist_norm)
+    boxplot_stats = compute_extended_boxplot_stats(dist_norm)
     fig, ax = plt.subplots()
     plot_extended_boxplot(ax, [boxplot_stats])
     fig.savefig('foo.png', dpi=200)
--- wats/plots.py
+++ wats/plots.py
@@ -25,7 +25,7 @@ sys.path.append(str(ROOT_DIR))
 
 from wats.util import init_logging
 from wats.nccmp import read_var, calc_rel_error
-from wats.boxplot import ExtendedBoxplotStats, plot_extended_boxplot
+from wats.ext_boxplot import compute_extended_boxplot_stats, plot_extended_boxplot
 
 BOXPLOT_VAR_NAMES = [
     'pressure',
@@ -58,12 +58,24 @@ def normalise(arr: np.array) -> np.array:
     norm = (arr - arr.min()) / (arr.max() - arr.min())
     return norm
 
+def compute_boxplot_stats(arr: np.array, label=None) -> dict:
+    boxplot_stats = cb.boxplot_stats(arr, whis=[2, 98], labels=[label])
+    assert len(boxplot_stats) == 1
+    boxplot_stats = boxplot_stats[0]
+    # There may be many outliers very close together.
+    # This increases memory usage for plotting considerably and increases data size.
+    # Let's remove all duplicates that we don't need.
+    boxplot_stats['fliers'] = np.unique(boxplot_stats['fliers'].round(decimals=5))
+    return boxplot_stats
+
 def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> None:
     logging.info('Reading reference and trial data for analysis')
 
     var_ref_all = {var_name: [] for var_name in KL_DIV_VAR_NAMES}
     var_trial_all = {var_name: [] for var_name in KL_DIV_VAR_NAMES}
     rel_errs = []
+    boxplot_stats_per_file = {}
+    ext_boxplot_stats_per_file = {}
     for ref_path in ref_dir.glob('wrf/*/wrfout_*'):
         rel_path = ref_path.relative_to(ref_dir)
         trial_path = trial_dir / rel_path
@@ -80,6 +92,8 @@ def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> N
             var_ref_all[var_name].append(var_ref.ravel())
             var_trial_all[var_name].append(var_trial.ravel())
 
+        boxplot_stats_per_var = []
+        ext_boxplot_stats_per_var = []
         for var_name in BOXPLOT_VAR_NAMES:
             logging.info(f'  Summary statistics: reading {var_name} & computing relative error')
             var_ref = read_var(nc_ref, var_name)
@@ -89,7 +103,14 @@ def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> N
                 logging.warn('  Found {} ref values < 0.01. Min: {}, Max: {}'.format(
                     small, np.min(var_ref), np.max(var_ref)))
             rel_err = calc_rel_error(var_ref, var_trial)
-            rel_errs.append(rel_err.ravel())
+            rel_err = rel_err.ravel()
+            rel_errs.append(rel_err)
+            logging.info(f'  Summary statistics: computing per-quantity boxplot stats')
+            boxplot_stats_per_var.append(compute_boxplot_stats(rel_err, label=var_name))
+            ext_boxplot_stats_per_var.append(compute_extended_boxplot_stats(rel_err, label=var_name))
+        boxplot_stats_per_file[str(rel_path)] = boxplot_stats_per_var
+        ext_boxplot_stats_per_file[str(rel_path)] = ext_boxplot_stats_per_var
+
     rel_errs = np.concatenate(rel_errs)
 
     logging.info('All data read')
@@ -108,17 +129,11 @@ def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> N
         kl_div = scipy.stats.entropy(trial_freq, ref_freq)
         kl_divs.append(kl_div)
 
-    logging.info('Computing boxplot stats')
-    boxplot_stats = cb.boxplot_stats(rel_errs, whis=[2, 98])
-    assert len(boxplot_stats) == 1
-    boxplot_stats = boxplot_stats[0]
-    # There may be many outliers very close together.
-    # This increases memory usage for plotting considerably and increases data size.
-    # Let's remove all duplicates that we don't need.
-    boxplot_stats['fliers'] = np.unique(boxplot_stats['fliers'].round(decimals=5))
+    logging.info('Computing boxplot stats (combined)')
+    boxplot_stats = compute_boxplot_stats(rel_errs)
 
-    logging.info('Computing extended boxplot stats')
-    extended_boxplot_stats = ExtendedBoxplotStats(rel_errs)
+    logging.info('Computing extended boxplot stats (combined)')
+    ext_boxplot_stats = compute_extended_boxplot_stats(rel_errs)
 
     logging.info('Storing stats')
     stats_path = out_dir / 'stats.pkl'
@@ -129,7 +144,10 @@ def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> N
         stats = []
 
     trial_name = trial_dir.name
-    stats.append((trial_name, boxplot_stats, extended_boxplot_stats, kl_divs))
+    stats.append((trial_name,
+                  boxplot_stats, boxplot_stats_per_file,
+                  ext_boxplot_stats, ext_boxplot_stats_per_file,
+                  kl_divs))
 
     with open(stats_path, 'wb') as fp:
         pickle.dump(stats, fp)
@@ -137,7 +155,7 @@ def compute_and_append_stats(ref_dir: Path, trial_dir: Path, out_dir: Path) -> N
 def plot(stats_dir: Path, out_dir: Path) -> None:
     stats_path = stats_dir / 'stats.pkl'
     boxplot_path = out_dir / 'boxplot.png'
-    ext_boxplot_path = out_dir / 'extended_boxplot.png'
+    ext_boxplot_path = out_dir / 'ext_boxplot.png'
     kl_div_path = out_dir / 'kl_div.png'
 
     logging.info('Loading stats')
@@ -150,22 +168,57 @@ def plot(stats_dir: Path, out_dir: Path) -> None:
 
     trial_names = [s[0] for s in stats]
     boxplot_stats_all_trials = [s[1] for s in stats]
-    ext_boxplot_stats_all_trials = [s[2] for s in stats]
-    kl_divs_all_trials = np.asarray([s[3] for s in stats])
+    boxplot_stats_all_trials_per_file = [s[2] for s in stats]
+    ext_boxplot_stats_all_trials = [s[3] for s in stats]
+    ext_boxplot_stats_all_trials_per_file = [s[4] for s in stats]
+    kl_divs_all_trials = np.asarray([s[5] for s in stats])
 
     for trial_idx, trial_name in enumerate(trial_names):
         print(f'{trial_idx}: {trial_name}')
 
-    logging.info('Creating boxplot')
+    logging.info('Creating boxplots (per-trial)')
     boxplot_fig, boxplot_ax = plt.subplots(figsize=(10,6),  dpi=300)
     boxplot_ax.set_xlabel('Trial number')
-    boxplot_ax.set_ylabel(r'$\eta$' + '/1')
+    boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
     sns.despine(boxplot_fig)
     boxplot_ax.bxp(boxplot_stats_all_trials)
     boxplot_ax.set_xticklabels(range(len(trial_names)))
     boxplot_fig.savefig(boxplot_path)
+    plt.close(boxplot_fig)
 
-    logging.info('Creating extended boxplot')
+    logging.info('Creating boxplots (per-trial per-file per-quantity)')
+    for trial_idx, boxplot_stats_per_file in enumerate(boxplot_stats_all_trials_per_file):
+        for rel_path, boxplot_stats_all_vars in boxplot_stats_per_file.items():
+            boxplot_fig, boxplot_ax = plt.subplots(figsize=(10,6))
+            trial_name = trial_names[trial_idx].replace('wats_', '')
+            boxplot_ax.set_title('Trial: {}\nFile: {}'.format(trial_name, rel_path))
+            boxplot_ax.set_xlabel('Quantity')
+            boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
+            sns.despine(boxplot_fig)
+            boxplot_ax.bxp(boxplot_stats_all_vars)
+            clean_rel_path = rel_path.replace('/', '_').replace('\\', '_')
+            boxplot_path = out_dir / 'boxplot_{}_{}.png'.format(trial_name, clean_rel_path)
+            boxplot_fig.savefig(boxplot_path)
+            plt.close(boxplot_fig)
+
+    # plot each quantity in a single plot for even greater detail
+    for trial_idx, boxplot_stats_per_file in enumerate(boxplot_stats_all_trials_per_file):
+        for rel_path, boxplot_stats_all_vars in boxplot_stats_per_file.items():
+            for boxplot_stats in boxplot_stats_all_vars:
+                boxplot_fig, boxplot_ax = plt.subplots(figsize=(10,6))
+                trial_name = trial_names[trial_idx].replace('wats_', '')
+                boxplot_ax.set_title('Trial: {}\nFile: {}'.format(trial_name, rel_path))
+                boxplot_ax.set_xlabel('Quantity')
+                boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
+                sns.despine(boxplot_fig)
+                boxplot_ax.bxp([boxplot_stats])
+                clean_rel_path = rel_path.replace('/', '_').replace('\\', '_')
+                quantity_name = boxplot_stats['label']
+                boxplot_path = out_dir / 'boxplot_{}_{}_{}.png'.format(trial_name, clean_rel_path, quantity_name)
+                boxplot_fig.savefig(boxplot_path)
+                plt.close(boxplot_fig)
+
+    logging.info('Creating extended boxplots (per-trial)')
     ext_boxplot_fig, ext_boxplot_ax = plt.subplots(figsize=(10,6),  dpi=300)
     ext_boxplot_ax.set_xlabel('Trial number')
     ext_boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
@@ -173,9 +226,43 @@ def plot(stats_dir: Path, out_dir: Path) -> None:
     plot_extended_boxplot(ext_boxplot_ax, ext_boxplot_stats_all_trials)
     ext_boxplot_ax.set_xticklabels(range(len(trial_names)))
     ext_boxplot_fig.savefig(ext_boxplot_path)
+    plt.close(ext_boxplot_fig)
+
+    logging.info('Creating extended boxplots (per-trial per-file per-quantity)')
+    for trial_idx, ext_boxplot_stats_per_file in enumerate(ext_boxplot_stats_all_trials_per_file):
+        for rel_path, ext_boxplot_stats_all_vars in ext_boxplot_stats_per_file.items():
+            ext_boxplot_fig, ext_boxplot_ax = plt.subplots(figsize=(10,6))
+            trial_name = trial_names[trial_idx].replace('wats_', '')
+            ext_boxplot_ax.set_title('Trial: {}\nFile: {}'.format(trial_name, rel_path))
+            ext_boxplot_ax.set_xlabel('Quantity')
+            ext_boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
+            sns.despine(ext_boxplot_fig)
+            plot_extended_boxplot(ext_boxplot_ax, ext_boxplot_stats_all_vars)
+            clean_rel_path = rel_path.replace('/', '_').replace('\\', '_')
+            ext_boxplot_path = out_dir / 'ext_boxplot_{}_{}.png'.format(trial_name, clean_rel_path)
+            ext_boxplot_fig.savefig(ext_boxplot_path)
+            plt.close(ext_boxplot_fig)
+
+    # plot each quantity in a single plot for even greater detail
+    for trial_idx, ext_boxplot_stats_per_file in enumerate(ext_boxplot_stats_all_trials_per_file):
+        for rel_path, ext_boxplot_stats_all_vars in ext_boxplot_stats_per_file.items():
+            for ext_boxplot_stats in ext_boxplot_stats_all_vars:
+                ext_boxplot_fig, ext_boxplot_ax = plt.subplots(figsize=(10,6))
+                trial_name = trial_names[trial_idx].replace('wats_', '')
+                ext_boxplot_ax.set_title('Trial: {}\nFile: {}'.format(trial_name, rel_path))
+                ext_boxplot_ax.set_xlabel('Quantity')
+                ext_boxplot_ax.set_ylabel(r'$\mathbf{\eta}$' + '/1')
+                sns.despine(ext_boxplot_fig)
+                plot_extended_boxplot(ext_boxplot_ax, [ext_boxplot_stats])
+                clean_rel_path = rel_path.replace('/', '_').replace('\\', '_')
+                quantity_name = ext_boxplot_stats['label']
+                ext_boxplot_path = out_dir / 'ext_boxplot_{}_{}_{}.png'.format(trial_name, clean_rel_path, quantity_name)
+                ext_boxplot_fig.savefig(ext_boxplot_path)
+                plt.close(ext_boxplot_fig)
 
     logging.info('Creating Kullback-Leibler divergence plot')
-    assert kl_divs_all_trials.shape == (len(trial_names), len(KL_DIV_VAR_NAMES))
+    assert kl_divs_all_trials.shape == (len(trial_names), len(KL_DIV_VAR_NAMES)), \
+        '{} != {}'.format(kl_divs_all_trials.shape, (len(trial_names), len(KL_DIV_VAR_NAMES)))
     if len(trial_names) > 1:
         # normalise each quantity to [0,1]
         for quantity_idx in range(len(KL_DIV_VAR_NAMES)):
@@ -196,7 +283,7 @@ def plot(stats_dir: Path, out_dir: Path) -> None:
         kl_div_var_labels.append(name + r'$_{\mathrm{Make}}$')
         kl_div_var_labels.append(name + r'$_{\mathrm{CMake}}$')
 
-    kl_fig, kl_ax = plt.subplots(figsize=(10,6),  dpi=300)
+    kl_fig, kl_ax = plt.subplots(figsize=(10,6), dpi=300)
     for trial_idx, kl_divs in enumerate(kl_divs_all_trials):
         trial = parse_trial_name(trial_names[trial_idx])
 
@@ -218,6 +305,7 @@ def plot(stats_dir: Path, out_dir: Path) -> None:
     kl_ax.set_xlabel('Quantity')
     kl_ax.set_ylabel(r'$\hat{\nabla}_{\mathrm{KL}}$'+ '/1')
     kl_fig.savefig(kl_div_path, dpi=200)
+    plt.close(kl_fig)
 
 if __name__ == '__main__':
     init_logging()
